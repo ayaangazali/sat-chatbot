1dbf9aae9e773d4882710d2b4ea22b951a9e1e62
ayaangazali <ayaangazali.work@gmail.com>
2022-04-07 06:40:52 -0700

    load key with dotenv

--- sat_bot.py
+++ sat_bot.py
@@ -1,7 +1,25 @@
 """SAT chatbot - helps answer and quiz SAT practice questions."""
 
+import os
+
+from dotenv import load_dotenv
+import openai
+
+
+def setup():
+    """Load the API key from the environment."""
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    if not api_key:
+        print("No OPENAI_API_KEY found. Copy .env.example to .env and add your key.")
+        return False
+    openai.api_key = api_key
+    return True
+
 
 def main():
+    if not setup():
+        return
     print("SAT bot is starting up.")
     while True:
         choice = input("Pick a mode> ").strip().lower()
