24cb5f8b48a831e89496ebc1f93afa9737a89a8b
ayaangazali <ayaangazali.work@gmail.com>
2022-06-01 11:27:59 -0700

    load questions function

--- sat_bot.py
+++ sat_bot.py
@@ -1,9 +1,12 @@
 """SAT chatbot - helps answer and quiz SAT practice questions."""
 
+import json
 import os
 
 from dotenv import load_dotenv
 import openai
+
+QUESTIONS_FILE = "questions.json"
 
 
 def setup():
@@ -17,10 +20,24 @@ def setup():
     return True
 
 
+def load_questions(path=QUESTIONS_FILE):
+    """Load the SAT question bank from a JSON file."""
+    try:
+        with open(path, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"Could not find the question bank at {path}.")
+        return []
+    except json.JSONDecodeError:
+        print(f"The question bank at {path} is not valid JSON.")
+        return []
+
+
 def main():
     if not setup():
         return
-    print("SAT bot is starting up.")
+    questions = load_questions()
+    print(f"SAT bot is ready. Loaded {len(questions)} questions.")
     while True:
         choice = input("Pick a mode> ").strip().lower()
         if choice == "/quit":
